8edcbd312619d0a7b4b65d90de3e0e575f11972c
Gevorg Ghalachyan <gevghalachyan@gmail.com>
2022-09-18 21:12:34 +0400

    lecture 3 assignment

---
+++ homework/assignment/lecture_3.py
@@ -0,0 +1,90 @@
+"""
+HW for the following topics
+    - built-in data types (lists, tuples)
+    - control flows
+Fill your from next line after # Answer:
+"""
+
+
+###################
+# Task 1: Write a program to sum all the items in a list [1,2,3,4,5,4,3,2,1]
+# Answer:
+
+###################
+###################
+# Task 2: Write a program to multiply all the items in a list [1,2,3,4,5,4,3,2,1]
+# Answer:
+
+###################
+###################
+# Task 3: Write a program to get the largest number from a list [1,2,3,4,5,4,3,2,1]
+# Answer:
+
+###################
+###################
+# Task 4: Write a program to get the index of the smallest number from a list [1,2,3,4,5,4,3,2,1]
+# Answer:
+
+###################
+###################
+# Task 5: Write a program to find the even numbers from a list [1,2,3,4,5,4,3,2,1]
+# Answer:
+
+###################
+###################
+# Task 6: Write a program to remove the odd numbers from a list [1,2,3,4,5,4,3,2,1]
+# Answer:
+
+###################
+###################
+# Task 7: Write a program to remove the odd numbers from a list [1,2,3,4,5,4,3,2,1]
+# Answer:
+
+###################
+###################
+# Task 8: Write a program to find if the input word/sentence is a palindrome, example - racecar, level
+# Answer:
+
+###################
+###################
+# Task 9: Create a 3X3 identity matrix using tuples
+# Answer:
+
+###################
+###################
+# Task 10: Write a program to check if gives matrix is an identity matrix
+# Answer:
+
+###################
+###################
+# Task 11: Write a program to return a tuple with square root of the numbers in tuple
+# Answer:
+
+###################
+###################
+# Task 12: Write a program to return the number of all possible pairs of elements in the list
+# Answer:
+
+###################
+###################
+# Task 13: Write a program to return the number of all possible pairs of elements in the list (return list of tuples)
+# Answer:
+
+###################
+###################
+# Task 14: Write a  program to generate all permutations of a list
+# Answer:
+
+###################
+###################
+# Task 15: Write a program to convert a list of numbers into a sorted list of unique values
+# Answer:
+
+###################
+###################
+# Task 16: Write a program to concatenate two lists of strings.
+# Example: ["a","b","c"], [1,2,3] -> ["a1","b2" ,"c3"]
+# if lengths of the lists differ, add "_" as a suffix/prefix
+# Answer:
+
+###################
