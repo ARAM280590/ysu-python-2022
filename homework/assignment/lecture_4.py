09f32244662ce7bb4df905e95690447814b0ebf4
Gevorg Ghalachyan <gevghalachyan@gmail.com>
2022-09-25 22:03:17 +0400

    lecture 4: hw

---
+++ homework/assignment/lecture_4.py
@@ -0,0 +1,47 @@
+"""
+HW for the following topics
+    - built-in data types (lists, tuples)
+    - control flows
+Fill your from next line after # Answer:
+"""
+
+#### BLACKJACK ####
+###################
+# Task 1:
+# Create and print 2 sets: ranks - ranks of cards; suits - suits of cards
+# Answer:
+
+###################
+###################
+# Task 2:
+# Creat and print a list of tuples: deck - deck of cards, using ranks and suits sets:
+# [("2", "spades"), ("3", "spades"),...]
+# Hint: use nested loops
+# Answer:
+
+###################
+###################
+# Task 3:
+# Check if the length of the deck is equal to 52
+# Answer:
+
+###################
+###################
+# Task 4:
+# Print top card of the deck
+# Shuffle the deck of of cards
+# Print top card of the deck
+# Hint: use random module
+# Answer:
+from random import shuffle
+
+###################
+###################
+# Task 5:
+# Create hand_values dictionary of ranks to soft hand and hard hand value: {rank: (soft_hand, hard_hand)}
+# Print the result
+# Example: {"ace": (1,11), "2": (2,2),...}
+# Hint: use the following logic - for ace it is (1,11), for a number it is the the face value, for the rest it is 10
+# Answer:
+
+###################
