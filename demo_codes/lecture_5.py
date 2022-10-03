25fc2dc0754a332e2f6dfd355d63fdf4fd0c07b9
Gevorg Ghalachyan <gevghalachyan@gmail.com>
2022-10-03 22:09:04 +0400

    lecture 5: hw assignment

--- demo_codes/lecture_5.py
+++ demo_codes/lecture_5.py
@@ -12,20 +12,25 @@ Materials
 
 
 #
-# # 1. so far you have been working with objects with are type of some class
+# 1. so far you have been working with objects with are type of some class
 # a = 1
 # print(type(a))
+# print(dir(a))
+# a < 1
+# a.__lt__(1)
 #
 #
-# # 2. create your first class (attributes and methods)
+# 2. create your first class (attributes and methods)
 # class Dog:  # defining a Dog class
 #
 #     def bark(self):  # defining a bark method
 #         print("bark!")
-#
-#
+# #
+# #
 # a_dog = Dog()  # instantiating an Dog class object
+# print(type(a_dog))
 # a_dog.bark()  # calling .bark() method for the instance
+#
 #
 # print(type(a_dog))  # remember __main__ module, __name__ variable
 # print(a_dog)
@@ -33,39 +38,42 @@ Materials
 #
 # # 3. initialize with __init__ SPECIAL method
 # # this method is automatically called once the class is created
-#
+
 # class Dog:
 #     """
 # Here is the docstring of your class
 # where you can and most preferably should add doc of the class
 # multiline is supported
 # 	"""
+#
 #     # here are some class attributes
 #     number_of_legs = 4
 #     number_of_eyes = 2
 #     family = "canidae"
 #
-#     def __init__(self, name, age):
+#     def __init__(self, name: str, age: int):
 #         """
 # When creating a new Dog class object it MUST
 # have a name name of the dog
 #
 # 		"""
 #         self.name = name  # here we assign a function argument to an instance, identifiers may not match
-#         self.age = age
+#         self.age = age  # type hinting
 #
-#     def bark(self):
+#     def bark(self) -> None:
 #         print("bark!")
 #
 #
 # a_dog = Dog("Max", 3)
+# print(type(a_dog.age))
 # print(a_dog.number_of_eyes)
 # print(a_dog.name)
-# print(Dog.bark())
-#
+# # print(Dog.bark())
+
 #
 # # 4. changing attributes
 #
+
 # class Dog:
 #     number_of_legs = 4
 #
@@ -90,7 +98,7 @@ Materials
 # a_dog.lose_a_leg()
 # a_dog.lose_a_leg()
 # print(f"Now {a_dog.name} has {a_dog.number_of_legs} legs")
-#
+
 #
 # # 5. let's code now
 # # class Student - init with name, age, prev_grade
@@ -116,7 +124,7 @@ Materials
 #     def set_university_year(self, university, year):
 #         self.university = university
 #         self.year = year
-#
+# #
 #
 # class Driver(Person):
 #
@@ -124,8 +132,8 @@ Materials
 #         self.licence_num = licence_num
 #         self.experience = experience
 #         self.cars = cars
-#
-#
+# #
+# #
 # st = Student(name="Ann", age=20)
 # st.set_university_year("YSU", 2)
 #
@@ -135,7 +143,7 @@ Materials
 #
 #
 # # 7. super.__init__(), superclass or upper-level class or parent class
-#
+
 # class Person:
 #     number_of_legs = 2
 #     number_of_eyes = 2
@@ -160,52 +168,52 @@ Materials
 # st.introduce()
 # print(f"I have {st.number_of_legs} legs")
 # print(f"Year of studies: {st.year}")
-#
-#
+
+
 # # decorators (functional programming)
 #
-# # 8. class methods
-# # self keyword is a reference to instance attributes
-# # cls keyword is a reference to class attributes
-#
-# class Student:
-#     number_of_students = 0
-#
-#     def __init__(self, name, university):
-#         self.name = name
-#         self.university = university
-#         Student.add_student()
-#
-#     @classmethod
-#     def add_student(cls):
-#         cls.number_of_students += 1
-#
-#
-# print(f"Current number of students: {Student.number_of_students}")
-# print(f"Current number of students: {Student.number_of_students}")
-# st1 = Student("Al", "TUM")
-# print(f"Current number of students: {Student.number_of_students}")
-# print(f"Current number of students: {st1.number_of_students}")
-# st2 = Student("Berg", "EPFL")
-# Student("a", 5)
-# print(f"Current number of students: {Student.number_of_students}")
-# print(f"Current number of students: {st1.number_of_students}")
-# print(f"Current number of students: {st2.number_of_students}")
-#
+# 8. class methods
+# self keyword is a reference to instance attributes
+# cls keyword is a reference to class attributes
+
+class Student:
+    number_of_students = 0
+
+    def __init__(self, name, university):
+        self.name = name
+        self.university = university
+        Student.add_student()
+
+    @classmethod
+    def add_student(cls):
+        cls.number_of_students += 1
+
+
+print(f"Current number of students: {Student.number_of_students}")
+print(f"Current number of students: {Student.number_of_students}")
+st1 = Student("Al", "TUM")
+print(f"Current number of students: {Student.number_of_students}")
+print(f"Current number of students: {st1.number_of_students}")
+st2 = Student("Berg", "EPFL")
+Student("a", 5)
+print(f"Current number of students: {Student.number_of_students}")
+print(f"Current number of students: {st1.number_of_students}")
+print(f"Current number of students: {st2.number_of_students}")
+
 #
 # # 9.static methods
 # # static methods act like functions
 # # they don't access anything, they don't mutate anything
 # # let's call this module from another script
 #
-# class MyFunctions:
-#
-#     @staticmethod
-#     def upper(x):
-#         if isinstance(x, str):
-#             return x.upper()
-#
-#     @staticmethod
-#     def capitalize(x):
-#         if isinstance(x, str):
-#             return x.capitalize()
+class MyFunctions:
+
+    @staticmethod
+    def upper(x):
+        if isinstance(x, str):
+            return x.upper()
+
+    @staticmethod
+    def capitalize(x):
+        if isinstance(x, str):
+            return x.capitalize()
---
+++ homework/assignment/lecture_5.py
@@ -0,0 +1,64 @@
+"""
+HW for the following topics
+    - built-in data types
+    - control flows
+    - function
+"""
+
+
+###################
+# Task 1: Write a function to return True if n is multiple of m, and False if otherwise
+# Answer:
+def is_multiple(n: int, m: int) -> bool:
+    pass
+
+###################
+###################
+# Task 2: Write a function to multiply two matrices(list of lists)
+# Answer:
+def matrix_multiply(a: list, b: list) -> list:
+    pass
+
+###################
+###################
+# Task 3: Write a list comprehension to generate the following list [0, 2, 6, 12, 20, 30, 42, 56, 72, 90]
+# Answer:
+
+###################
+###################
+# Task 4: Write a function that transforms large text of string into a list of separate list of sentences
+# Answer:
+def text_to_sentences(text: str) -> list:
+    pass
+
+###################
+###################
+# Task 5: Write a function to print total number of goods in basket and total cost of the goods in basket where basket
+# is given as a keyword arbitrary parameter
+# Answer:
+def basket_aggregator():
+    pass
+
+###################
+###################
+# Task 6: a, b, c are the height of a triangle, ab, ac, bc are the angles, h is the height, s is the area. Any of them are given as a keyword
+# arbitrary parameter. Find all parameters that is not given and are possible to find.
+# Answer:
+def triangle_solver():
+    pass
+
+###################
+###################
+# Task 7: Write a function to compute frequencies of a each word in a text (Do not use a build-in or an imported function)
+# Answer:
+def freq_counter(text: str) -> dict:
+    pass
+
+###################
+###################
+# Task 8: Write a function to print statistics(mean, std, min, q1, median, q3, q95%, max) of a list of floats.
+# Answer:
+def statistics(ls: list) -> None:
+    pass
+
+###################
